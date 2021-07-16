59d1e3418354c48bcca019c01291cc8327ad5dd4
Abby Hilman <abbyhilman@gmail.com>
2021-07-16 22:23:13 +0700

    first-commit

--- src/App.js
+++ src/App.js
@@ -2,8 +2,6 @@ import 'react-native-gesture-handler';
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import Route from './route';
-import {Provider} from 'mobx-react';
-import store from './mobx/store';
 
 const App = () => {
   return (
